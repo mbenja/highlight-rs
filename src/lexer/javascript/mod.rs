fd224f8fd56ce5e8c1e515063c654d74a2db39b1
Benjamin Streit <benjaminmstreit@gmail.com>
2022-06-15 21:47:43 -0500

    Parsing numeric literals in JavaScript

--- src/lexer/javascript/mod.rs
+++ src/lexer/javascript/mod.rs
@@ -11,6 +11,7 @@ pub enum JavaScriptTokenKind {
   Identifier,
   Keyword,
   Null,
+  Number,
   String,
 }
 
@@ -62,8 +63,9 @@ const KEYWORDS: [&str; 43] = [
 
 pub fn lex_javascript(lexer: &mut Lexer) {
   while lexer.current_position < lexer.input.len() {
-    if let Some(token) = read_next_token(lexer) {
-      lexer.tokens.push(token);
+    match read_next_token(lexer) {
+      Some(token) => lexer.tokens.push(token),
+      None => progress_to_whitespace(lexer),
     }
   }
 }
@@ -109,10 +111,18 @@ fn read_next_token(lexer: &mut Lexer) -> Option<Token> {
         span,
       ));
     }
-  } else if next_char == &'/' {
-    let second_char = *lexer.input.get(lexer.current_position + 1)?;
+  } else if next_char.is_digit(10) {
+    return read_numeric_literal(lexer);
+  } else if next_char == &'-' {
+    let second_char = lexer.input.get(lexer.current_position + 1)?;
 
-    if second_char == '*' {
+    if second_char.is_digit(10) {
+      return read_numeric_literal(lexer);
+    }
+  } else if next_char == &'/' {
+    let second_char = lexer.input.get(lexer.current_position + 1)?;
+
+    if second_char == &'*' {
       return Some(read_multi_line_comment(lexer));
     } else {
       return Some(read_single_line_comment(lexer));
@@ -124,6 +134,17 @@ fn read_next_token(lexer: &mut Lexer) -> Option<Token> {
   }
 
   None
+}
+
+fn progress_to_whitespace(lexer: &mut Lexer) {
+  while let Some(first_char) = lexer.input.get(lexer.current_position) {
+    if !first_char.is_whitespace() {
+      lexer.current_position += 1;
+      continue;
+    } else {
+      break;
+    }
+  }
 }
 
 fn progress_to_non_whitespace(lexer: &mut Lexer) {
@@ -189,6 +210,106 @@ fn read_string_literal(lexer: &mut Lexer) -> Option<Token> {
 
   Some(Token::new(
     TokenKind::JavaScript(JavaScriptTokenKind::String),
+    Span::new(start_position, lexer.current_position - 1),
+  ))
+}
+
+fn read_numeric_literal(lexer: &mut Lexer) -> Option<Token> {
+  const PERMISSABLE_NON_NUMERIC_CHARS: [char; 10] =
+    ['n', 'e', 'b', '-', '+', 'o', 'O', 'x', 'X', '.'];
+  let start_position = lexer.current_position;
+  let mut prev_char = *lexer.input.get(lexer.current_position)?;
+  let mut radix = 10;
+  let mut last_separator_position = 0;
+  let mut is_float = false;
+
+  while let Some(next_char) = lexer.input.get(lexer.current_position) {
+    if next_char.is_whitespace() || next_char == &';' {
+      break;
+    }
+
+    if (next_char == &'b' || next_char == &'B')
+      && prev_char == '0'
+      && lexer.current_position == start_position + 1
+    {
+      radix = 2;
+    } else if (next_char == &'o' || next_char == &'O')
+      && prev_char == '0'
+      && lexer.current_position == start_position + 1
+    {
+      radix = 8;
+    } else if (next_char == &'x' || next_char == &'X')
+      && prev_char == '0'
+      && lexer.current_position == start_position + 1
+    {
+      radix = 16;
+    }
+
+    if (next_char == &'-' || next_char == &'+')
+      && (prev_char != 'e' || !lexer.input.get(lexer.current_position + 1)?.is_digit(radix))
+    {
+      return None; // Invalid exponential literal format
+    } else if (next_char == &'b' || next_char == &'B') && prev_char != '0' && radix != 16 {
+      return None; // Invalid placement of b character for binary literal
+    } else if (next_char == &'o' || next_char == &'O') && prev_char != '0' {
+      return None; // Invalid placement of o character for octal literal
+    } else if (next_char == &'x' || next_char == &'X') && prev_char != '0' {
+      return None; // Invalid placement of x character for hexadecimal literal
+    } else if next_char == &'_' && prev_char == '_' {
+      return None; // Double numeric separator is not allowed
+    } else if next_char == &'_' && prev_char == '0' && lexer.current_position == start_position + 1
+    {
+      return None; // Numeric separator after leading 0 is not allowed
+    } else if next_char == &'_' {
+      if radix == 10
+        && lexer.current_position != start_position + 1
+        && lexer.current_position - 4 != last_separator_position
+      {
+        return None; // Invalid placement of numeric separator in base 10 literal
+      } else if radix == 2
+        && lexer.current_position != start_position + 6
+        && lexer.current_position - 5 != last_separator_position
+      {
+        return None; // Invalid placement of numeric separator in binary literal
+      } else if radix == 8
+        && lexer.current_position != start_position + 3
+        && lexer.current_position - 2 != last_separator_position
+      {
+        return None; // Invalid placement of numeric separator in octal literal
+      } else if radix == 16
+        && lexer.current_position != start_position + 4
+        && lexer.current_position - 3 != last_separator_position
+      {
+        return None; // Invalid placement of numeric separator in hexadecimal literal
+      }
+    } else if prev_char == 'n' {
+      return None; // Invalid placement of n character for BigInt literal
+    } else if next_char == &'.' && is_float {
+      return None; // Invalid float literal format
+    } else if !next_char.is_digit(radix) && !PERMISSABLE_NON_NUMERIC_CHARS.contains(next_char) {
+      return None; // Passes above checks but is badly formed numeric literal
+    }
+
+    if next_char == &'.' {
+      is_float = true;
+    }
+
+    if next_char == &'_' {
+      last_separator_position = lexer.current_position;
+    }
+
+    lexer.current_position += 1;
+    prev_char = *next_char;
+  }
+
+  if prev_char == '.' {
+    return None; // Invalid placement of decimal in float literal
+  } else if prev_char == '_' {
+    return None; // Ending numeric separator not allowed
+  }
+
+  Some(Token::new(
+    TokenKind::JavaScript(JavaScriptTokenKind::Number),
     Span::new(start_position, lexer.current_position - 1),
   ))
 }
--- tests/javascript_lexer_tests.rs
+++ tests/javascript_lexer_tests.rs
@@ -332,4 +332,641 @@ mod javascript_lexer_tests {
 
     assert_eq!(*lexer.get_tokens(), expected);
   }
+
+  #[test]
+  fn lexer_parses_unsigned_number_base_10() {
+    let input = String::from("let myVar = 123;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 14),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_exponential_numbers() {
+    let input = String::from("let myVar = 0e-5; let myVar = 0e+5; let myVar = 5e1;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 15),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(18, 20),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(22, 26),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(30, 33),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(36, 38),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(40, 44),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(48, 50),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_binary_number() {
+    let input = String::from("let myVar = 0b10;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 15),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_binary_number() {
+    let input = String::from("let myVar = 0b3;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_octal_number() {
+    let input = String::from("let myVar = 0o755; let myVar = 0O755;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 16),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(19, 21),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(23, 27),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(31, 35),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_octal_number() {
+    let input = String::from("let myVar = 0o9;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_hexadecimal_number() {
+    let input = String::from("let myVar = 0xA; let myVar = 0XA;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 14),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(17, 19),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(21, 25),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(29, 31),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_hexadecimal_number() {
+    let input = String::from("let myVar = 0xz;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_big_int_number() {
+    let input =
+      String::from("let myVar = 123n; let myVar = 0o7n; let myVar = 0xAn; let myVar = 0b1n;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 15),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(18, 20),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(22, 26),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(30, 33),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(36, 38),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(40, 44),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(48, 51),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(54, 56),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(58, 62),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(66, 69),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_big_int_number() {
+    let input = String::from("let myVar = 1n5;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_float_number() {
+    let input = String::from("let myVar = 1.5;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 14),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_float_number() {
+    let input = String::from("let myVar = 1.5.5;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_numeric_separator_in_base_10_number() {
+    let input = String::from("let myVar = 1_000_000;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 20),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_numeric_separator_in_float_number() {
+    let input = String::from("let myVar = 1_050.95;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 19),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_numeric_separator_in_base_10_number() {
+    let input = String::from("let myVar = 1_000_1_0");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_numeric_separator_in_binary_number() {
+    let input = String::from("let myVar = 0b1010_0001_1000;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 27),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_numeric_separator_in_binary_number() {
+    let input = String::from("let myVar = 0b1010_00_01_1000;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_numeric_separator_in_octal_number() {
+    let input = String::from("let myVar = 0o2_2_5_6;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 20),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_numeric_separator_in_octal_number() {
+    let input = String::from("let myVar = 0o2_2_55_6;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_numeric_separator_in_hexadecimal_number() {
+    let input = String::from("let myVar = 0xA0_B0_C0;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 21),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_invalid_numeric_separator_in_hexadecimal_number() {
+    let input = String::from("let myVar = 0xA0_B00_C0;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_parses_numeric_separator_in_big_int_number() {
+    let input = String::from("let myVar = 1_000_000n;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Number),
+        Span::new(12, 21),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_double_numeric_separator() {
+    let input = String::from("let myVar = 1_000__000;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_ending_numeric_separator() {
+    let input = String::from("let myVar = 1_000_000_;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
+
+  #[test]
+  fn lexer_skips_numeric_separator_after_leading_zero() {
+    let input = String::from("let myVar = 0_1;");
+    let expected = vec![
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Keyword),
+        Span::new(0, 2),
+      ),
+      Token::new(
+        TokenKind::JavaScript(JavaScriptTokenKind::Identifier),
+        Span::new(4, 8),
+      ),
+    ];
+
+    let mut lexer = Lexer::new(input);
+    lexer.process_input(Language::JavaScript);
+
+    assert_eq!(*lexer.get_tokens(), expected);
+  }
 }
